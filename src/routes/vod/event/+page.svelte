984cec79e0626ac9f23de3485480ceba8bbaa7b3
jeongmin <wjdals3255@gmail.com>
2023-11-07 23:41:18 +0900

    #STUDY: 이벤트 핸들링

---
+++ src/routes/vod/event/+page.svelte
@@ -0,0 +1,16 @@
+<!-- ST -->
+
+<script>
+  let text =''
+</script>
+
+<h1>
+  {text}
+</h1>
+
+<input 
+  type="text"  
+  value={text} 
+  on:input={(e) => {text = e.target.value}} 
+/>
+<button on:click={() => {text = 'jeongmin'}}>Click</button>
