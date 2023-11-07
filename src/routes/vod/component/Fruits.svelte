5c10d4b87f5f5fd3dd78f6694f33f92424cd02fb
jeongmin <wjdals3255@gmail.com>
2023-11-08 00:20:22 +0900

    #STUDY: component

---
+++ src/routes/vod/component/Fruits.svelte
@@ -0,0 +1,31 @@
+<script>
+  // Props
+  export let fruits
+  export let reverse
+  export let slice
+
+  /**
+	 * @type {any[]}
+	 */
+  let computedFruits = []
+  let name = ''
+
+  if(reverse){
+    computedFruits = [...fruits].reverse()
+    name = 'reverse'
+  }else if(slice) {
+    computedFruits = fruits.slice(...slice.split(','))
+    name = `slice ${slice}`
+  }else {
+    computedFruits = fruits
+  }
+</script>
+
+<h2>
+  Fruits {name}
+</h2>
+<ul>
+  {#each computedFruits as fruit }
+    <li>{fruit}</li>
+  {/each}
+</ul>
--- src/routes/vod/event/+page.svelte
+++ src/routes/vod/event/+page.svelte
@@ -1,4 +1,35 @@
-<!-- ST -->
+<!-- <script>
+  import {onMount} from 'svelte'
+  let name = 'world'
+  let isRed = false
+  // onMount(() => {
+  //   const box = document.querySelector('.box')
+  //   box?.addEventListener('click', () => {isRed = !isRed})
+
+  // })
+  function enter() {
+    name = 'enter'
+  }
+  function liave() {
+    name = 'liave'
+  }
+</script>
+
+<h1>Hello {name}!</h1>
+<div class="box" style="background-color: {isRed ? 'red' : 'orange'};" on:click={() => {isRed = !isRed}}
+  on:mouseenter={enter}
+  on:mouseleave={liave}
+  >
+  Box!
+</div>
+
+<style>
+  .box{
+    width: 300px;
+    height: 150px;
+    background-color: orange;
+  }
+</style> -->
 
 <script>
   let text =''
