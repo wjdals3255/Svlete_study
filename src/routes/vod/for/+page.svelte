a26c728f3a1829e366634fee6491ccd49f3d2742
jeongmin <wjdals3255@gmail.com>
2023-11-07 23:02:16 +0900

    #STUDY:조건문

---
+++ src/routes/vod/for/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  let name = 'world'
+  let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'] 
+</script>
+
+<h1>Hello {name}!</h1>
+<ul>
+  
+</ul>
+<!-- {#each}
+{/each} -->
---
+++ src/routes/vod/if/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  let name = 'world'
+  let toggle = false
+  if(toggle){
+    
+  }
+</script>
+
+<button on:click={() =>{toggle = !toggle}}>
+  Toggle
+</button>
+{#if toggle}
+  <h1>Hello {name}!</h1>
+{:else}
+  <div>No name !</div>
+{/if}
+<!-- 시작: "#", 중간구문: ":", 끝: "/" -->
