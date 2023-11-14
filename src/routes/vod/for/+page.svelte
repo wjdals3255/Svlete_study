5b8c017dfe2c781a851816907f1443d351e3833d
jeongmin <wjdals3255@gmail.com>
2023-11-14 09:55:39 +0900

    test

--- src/routes/vod/for/+page.svelte
+++ src/routes/vod/for/+page.svelte
@@ -1,15 +1,16 @@
 <script>
-  let name = 'Fruits'
-  let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'] 
-  function deleteFruit() {
-    fruits = fruits.slice(1)
-  }
+	let name = 'Fruits';
+	let fruits = ['Apple', 'Banana', 'Cherry', 'Orange', 'Mango'];
+	function deleteFruit() {
+		fruits = fruits.slice(1);
+		console.log(fruits);
+	}
 </script>
 
 <h1>Hello {name}!</h1>
 <ul>
-  {#each fruits as fruit}
-    <li>{fruit}</li>
-  {/each}
+	{#each fruits as fruit}
+		<li>{fruit}</li>
+	{/each}
 </ul>
 <button on:click={deleteFruit}>Eat it!</button>
